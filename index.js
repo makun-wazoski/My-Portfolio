0ba00b6c51aace768f73ca5f3c649e3999f0535e
makun-wazoski <mo19850226@gmail.com>
2021-10-02 19:04:03 +0900

    add hamburgerMenu

---
+++ index.js
@@ -0,0 +1,49 @@
+$(function() {
+  const $a = $('#a');
+  const $b = $('#b');
+  const $c = $('#c');
+  const $d = $('#d');
+  const $nav = $('#navArea');
+  const $btn = $('.toggle_btn');
+  const $mask = $('#mask');
+  const open = 'open';
+
+
+  $btn.on('click', function() {
+    if(!$nav.hasClass(open)) {
+      $nav.addClass(open);
+    } else {
+      $nav.removeClass(open);
+    }
+  });
+
+  $mask.on('click', function() {
+    $nav.removeClass(open);
+  });
+  $a.on('click', function () {
+    $nav.removeClass(open);
+  });
+  $b.on('click', function () {
+    $nav.removeClass(open);
+  });
+  $c.on('click', function () {
+    $nav.removeClass(open);
+  });
+  $d.on('click', function () {
+    $nav.removeClass(open);
+  });
+});
+
+// スムーズスクロール
+$(function(){
+  $('a[href^="#"]').click(function(){
+      var adjust = 0;
+      var speed = 1000;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top + adjust;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+  });
+});
+// スムーズスクロール
